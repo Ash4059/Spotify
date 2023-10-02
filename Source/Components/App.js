5c8a009d61a44066854c50b36b6e9a22d90b9706
zeuslearning-ashish-jaiswal <ashish.jaiswal@zeuslearning.com>
2023-10-02 18:02:04 +0530

    Pages added successfully....

--- Source/Components/App.js
+++ Source/Components/App.js
@@ -3,6 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Home } from "../Pages/Home"
 import { Login } from "../Pages/Login"
 import { Navbar } from './Navbar';
+import Music from '../Pages/Music';
+import LikedAudio from '../Pages/LikedAudio';
+import Bookmark from '../Pages/Bookmark';
+import Account from '../Pages/Account';
+import Podcast from '../Pages/Podcast';
 
 const router = createBrowserRouter([
     {
@@ -12,6 +17,30 @@ const router = createBrowserRouter([
     {
         path : '/login',
         element : <Login />
+    },
+    {
+        path : '/music',
+        element : <Music />
+    },
+    {
+        path : '/liked',
+        element : <LikedAudio />
+    },
+    {
+        path : '/podcast',
+        element : <Podcast />
+    },
+    {
+        path : '/bookmark',
+        element : <Bookmark />
+    },
+    {
+        path : '/history',
+        element : <History />
+    },
+    {
+        path : '/account',
+        element : <Account />
     }
 ])
 
---
+++ Source/Pages/LikedAudio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LikedAudio = () => {
+  return (
+    <div>LikedAudio</div>
+  )
+}
+
+export default LikedAudio
